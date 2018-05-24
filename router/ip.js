2556aec026deac08b58bfae61a79e5adf19cd905
chrunlee <chrunlee@foxmail.com>
2018-05-24 10:07:56 +0800

    ip

--- router/ip.js
+++ router/ip.js
@@ -4,12 +4,10 @@ var express = require('express');
 var Router = express.Router();
 
 Router.get('/',function(req,res,next){
-	var str = req.ip;
-	var ips = /[\d]{1,3}.[\d]{1,3}.[\d]{1,3}.[\d]{1,3}/.exec(str);
-	var ip = '';
-	if(ips && ips.length > 0){
-		ip = ips[0];
-	}
+	var ip = req.headers['x-forwarded-for'] || // 判断是否有反向代理 IP
+        req.connection.remoteAddress || // 判断 connection 的远程 IP
+        req.socket.remoteAddress || // 判断后端的 socket 的 IP
+        req.connection.socket.remoteAddress;
 	res.end(ip);
 });
 
